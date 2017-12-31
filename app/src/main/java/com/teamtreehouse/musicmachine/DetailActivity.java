8105592c8ae484d2b503fc3b785dc55c9b217ab4
bobtal <talevski.boban@gmail.com>
2017-12-31 16:54:15 +0100

    end of Handling an Intent from a Filter video

--- app/src/main/java/com/teamtreehouse/musicmachine/DetailActivity.java
+++ app/src/main/java/com/teamtreehouse/musicmachine/DetailActivity.java
@@ -2,11 +2,13 @@ package com.teamtreehouse.musicmachine;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.teamtreehouse.musicmachine.models.Song;
@@ -14,6 +16,7 @@ import com.teamtreehouse.musicmachine.models.Song;
 public class DetailActivity extends AppCompatActivity {
 
     private Song mSong;
+    private RelativeLayout rootLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,29 +25,47 @@ public class DetailActivity extends AppCompatActivity {
 
         TextView titleLabel = (TextView)findViewById(R.id.songTitleLabel);
         final CheckBox favoriteCheckbox = (CheckBox)findViewById(R.id.checkBox);
+        rootLayout = findViewById(R.id.rootLayout);
 
         Intent intent = getIntent();
+
+        // check if it's an implicit intent coming from "outside"
+        // or if it's an explicit intent coming from our app
+        if (intent.getAction().equals(Intent.ACTION_SEND)) {
+            // implicit intent coming from "outside"
+            handleSendIntent(intent);
+        } else {
+            // explicit intent coming from our app
+
 //        if (intent.getStringExtra(MainActivity.EXTRA_TITLE) != null) {
 //            String songTitle = intent.getStringExtra(MainActivity.EXTRA_TITLE);
 //            titleLabel.setText(songTitle);
 //        }
-        if (intent.getParcelableExtra(MainActivity.EXTRA_SONG) != null) {
-            Song song = intent.getParcelableExtra(MainActivity.EXTRA_SONG);
-            titleLabel.setText(song.getTitle());
-            favoriteCheckbox.setChecked(song.isFavorite());
-        }
-        int listPosition = intent.getIntExtra(MainActivity.EXTRA_LIST_POSITION, 0);
-
-        favoriteCheckbox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                Intent resultIntent = new Intent();
-                resultIntent.putExtra(MainActivity.EXTRA_FAVORITE, isChecked);
-                resultIntent.putExtra(MainActivity.EXTRA_LIST_POSITION, listPosition);
-                setResult(RESULT_OK, resultIntent);
-                finish();
+            if (intent.getParcelableExtra(MainActivity.EXTRA_SONG) != null) {
+                Song song = intent.getParcelableExtra(MainActivity.EXTRA_SONG);
+                titleLabel.setText(song.getTitle());
+                favoriteCheckbox.setChecked(song.isFavorite());
             }
-        });
+            int listPosition = intent.getIntExtra(MainActivity.EXTRA_LIST_POSITION, 0);
+
+            favoriteCheckbox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                @Override
+                public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                    Intent resultIntent = new Intent();
+                    resultIntent.putExtra(MainActivity.EXTRA_FAVORITE, isChecked);
+                    resultIntent.putExtra(MainActivity.EXTRA_LIST_POSITION, listPosition);
+                    setResult(RESULT_OK, resultIntent);
+                    finish();
+                }
+            });
+        }
+    }
+
+    private void handleSendIntent(Intent intent) {
+        if (intent.getStringExtra(Intent.EXTRA_TEXT) != null) {
+            Snackbar.make(rootLayout, intent.getStringExtra(Intent.EXTRA_TEXT),
+                    Snackbar.LENGTH_LONG).show();
+        }
     }
 
     @Override

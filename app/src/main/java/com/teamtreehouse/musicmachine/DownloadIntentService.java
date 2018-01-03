7d3b89ccef83d3347bc27a2e0ca7ab32cf44e88a
bobtal <talevski.boban@gmail.com>
2018-01-03 23:39:25 +0100

    implements extra task to tap on notifications to display that sond details in DetailsActivity

--- app/src/main/java/com/teamtreehouse/musicmachine/DownloadIntentService.java
+++ app/src/main/java/com/teamtreehouse/musicmachine/DownloadIntentService.java
@@ -3,6 +3,7 @@ package com.teamtreehouse.musicmachine;
 import android.app.IntentService;
 import android.app.Notification;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
@@ -11,6 +12,7 @@ import com.teamtreehouse.musicmachine.models.Song;
 
 public class DownloadIntentService extends IntentService {
     private static final String TAG = DownloadIntentService.class.getSimpleName();
+    private static final int REQUEST_OPEN_FROM_NOTIFICATION = 55;
 
     private NotificationManager notificationManager;
     private static final int NOTIFICATION_ID = 22;
@@ -24,10 +26,19 @@ public class DownloadIntentService extends IntentService {
     protected void onHandleIntent(Intent intent) {
         Song song = intent.getParcelableExtra(MainActivity.EXTRA_SONG);
 
+        Intent mainIntent = new Intent(this, DetailActivity.class);
+        mainIntent.putExtra(MainActivity.EXTRA_SONG, song);
+        PendingIntent pendingIntent = PendingIntent.getActivity(
+                        this,
+                        REQUEST_OPEN_FROM_NOTIFICATION,
+                        mainIntent,
+                        PendingIntent.FLAG_UPDATE_CURRENT);
+
         Notification.Builder builder = new Notification.Builder(this)
                 .setSmallIcon(R.drawable.ic_queue_music_white)
                 .setContentTitle("Downloading")
                 .setContentText(song.getTitle())
+                .setContentIntent(pendingIntent)
                 .setProgress(0, 0, true);
 
         notificationManager =
